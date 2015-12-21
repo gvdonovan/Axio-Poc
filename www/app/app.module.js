e289d71587daf8fde576decd1a6820a6ee9301d7
Greg Donovan <gdonovan@axiometricsinc.local>
2015-12-21 17:10:51 -0600

    tracking angular state changes

--- www/app/app.module.js
+++ www/app/app.module.js
@@ -1,10 +1,21 @@
 (function () {
     'use strict';
 
-    angular.module('app', [
+    var app = angular.module('app', [
         'app.core',
         'app.layout',
         'app.home',
         'app.admin'
     ]);
+
+    app.run(['$rootScope', '$window', '$state', function($rootScope, $window, $state) {
+
+        $rootScope.$on('$stateChangeSuccess', function(event){
+
+            if (!$window.ga)
+                return;
+
+            $window.ga('Biff.send', 'pageview', { page: $state.current.name });
+        });
+    }])
 })();
